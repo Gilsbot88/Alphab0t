28db2588d08885bedff43126f70d75d802b007fa
Gilsbot88 <101207495+Gilsbot88@users.noreply.github.com>
2022-03-25 20:32:48 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,14 +2,14 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+1. *Zona Gabut*
+_https://chat.whatsapp.com/I3uvRkj0vGF0yexwy1OnUv_
+2. *Gc bokep 1*
+_https://chat.whatsapp.com/Jvp57NiL4yq4dnOHr2x37c_
+3. *Gc bokep 2*
+_https://chat.whatsapp.com/CX14ENWgTqP8opmAGJOMWn_
+4. *Gc bokep 3* 
+_https://chat.whatsapp.com/J8jRHj8kyim2QEJvQSklKf_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
